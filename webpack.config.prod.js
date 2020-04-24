669c00c66d1a116f96950dea0d54ae33d5b6217f
GuiMac <guibasconti@gmail.com>
2020-04-24 00:52:01 -0300

    Add nature of code prefix to bundled assets

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,7 +24,7 @@ export default {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/nature-of-code/',
     filename: '[name].[contenthash].js'
   },
   plugins: [
